d96766dccf728bf6f8bde47a69b1a686367607f0
Abhishek Upperwal <a.upperwal@gmail.com>
2020-11-08 20:49:28 +0530

    added social media on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 import './Header.scss';
 
+import Twitter from './twitter.svg';
+import Instagram from './instagram.svg';
+import Linkedin from './linkedin.svg';
+
 function Header() {
     return (
         <section className="Header">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-2 social-media extra-padding">1</div>
+                    <div className="col-md-2 social-media extra-padding">
+                        <a href="https://twitter.com/SmartMove2020" target="_blank"><img src={Twitter} alt=""/></a>
+                        <a href="https://www.instagram.com/smartmove_2020/" target="_blank"><img src={Instagram} alt=""/></a>
+                        <a href="https://www.linkedin.com/company/smartmove2020" target="_blank"><img src={Linkedin} alt=""/></a>
+                    </div>
                     <div className="col-md-8"></div>
                     <div className="col-md-2 reg-link extra-padding"><a href="https://hack.dataspace.mobi">REGISTRATION</a></div>
                 </div>
