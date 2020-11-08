382eac19ca4a1b608d2ae56d8c208a93bcd38d2e
Abhishek Upperwal <a.upperwal@gmail.com>
2020-11-08 20:40:22 +0530

    header has its own space now

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Header from '../Header/Header';
+
 import './Home.scss';
 
 import Hero from './hero.svg'
@@ -10,28 +12,8 @@ import Logo from './smart-move-logo.svg'
 function Home() {
     return(
         <section className="Home">
-            <section className="Header">
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-2 social-media extra-padding">1</div>
-                        <div className="col-md-8"></div>
-                        <div className="col-md-2 reg-link extra-padding"><a href="https://hack.dataspace.mobi">REGISTRATION</a></div>
-                    </div>
-                    <div className="row bottom-bar">
-                        <div className="col-md-12">
-                            <ul>
-                                <li><a href="/">HOME</a></li>
-                                <li><a href="/">GUIDELINES</a></li>
-                                <li><a href="/">JURY</a></li>
-                                <li><a href="/">WINNERS</a></li>
-                                <li><a href="/">PARTNERS</a></li>
-                                <li><a href="/">FAQ</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
+            
+            <Header/>
 
             <section className="hero">
                 <div className="container">
