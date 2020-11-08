ff96ceb835fab910d2cc264176ebeb3c6d0cb6f2
Abhishek Upperwal <a.upperwal@gmail.com>
2020-11-09 00:39:40 +0530

    added social media in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 import './Footer.scss';
 
 import Logo from './smart-move-logo.svg'
+import TwitterLogo from './twitter.svg'
+import InstagramLogo from './instagram.svg'
+import LinkedinLogo from './linkedin.svg'
 
 function Footer() {
     return (
@@ -15,9 +18,13 @@ function Footer() {
                     <div className="col-md-6">
                         <small>For more details, contact us at: <b>smartmove@niua.org</b></small>
                         <small>National Institute of Urban Affairs, 1st and 2nd Floor, Core 4B, India Habitat Centre, Lodhi Road, New Delhi</small>
-                        <small>The SMART MOVE Challenge is a part of the Indian-German Green Urban Mobility Partnership</small>
+                        <small><b>The SMART MOVE Challenge is a part of the Indian-German Green Urban Mobility Partnership</b></small>
                     </div>
-                    <div className="col-md-3"></div>
+                    <div className="col-md-3 social-media">
+                        <a href="https://twitter.com/SmartMove2020" target="_blank"><img src={TwitterLogo} alt=""/></a>
+                        <a href="https://www.instagram.com/smartmove_2020/" target="_blank"><img src={InstagramLogo} alt=""/></a>
+                        <a href="https://www.linkedin.com/company/smartmove2020" target="_blank"><img src={LinkedinLogo} alt=""/></a>
+                    </div>
                 </div>
             </div>
         </section>
