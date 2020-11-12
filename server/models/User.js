c41d5f2c78dbd7dbdd987f3153029b76d12d9e34
Rishabh <rishabh.sisodiya4@gmail.com>
2020-11-13 05:24:49 +0530

    remove / route

--- server/models/User.js
+++ server/models/User.js
@@ -26,14 +26,12 @@ userSchema.pre('save', function (next) {
     var user = this;
 
     if (user.isModified('password')) {
-        console.log('password changed')
         bcrypt.genSalt(saltRounds, function (err, salt) {
             if (err) return next(err);
 
             bcrypt.hash(user.password, salt, function (err, hash) {
                 if (err) return next(err);
-                user.password = hash
-                // console.log('passowrd hash',hash);
+                user.password = hash;
                 next()
             })
         })
