94f817f6e91e2aae666d4333cb61d48486ad9a5f
Rishabh <rishabh.sisodiya4@gmail.com>
2020-11-27 07:35:55 +0530

    register user using pydio url

--- server/routes/users.js
+++ server/routes/users.js
@@ -66,56 +66,52 @@ router.post("/registerEmail", (req, res) => {
   let email = req.body.Email;
   let user = req.body.Poc;
   let docObject = req.body.doc;
-  // console.log('docObject: ',docObject);
-  // let buffer= Buffer.from(JSON.stringify(docObject));
-  // console.log(buffer);
-  // console.log(new Buffer('hello world!','utf-8'))
   //  Call api for register user
-  // const tokenUrl = "https://storage.dataspace.mobi/a/frontend/session";
-  // const url = "https://storage.dataspace.mobi/a/user/upperwal";
-  // const login = process.env.login;
-  // const password = process.env.password;
-  // const type = process.env.type;
-  // const authInfo = { login, password, type };
-  // axios
-  //   .post(tokenUrl, {
-  //     AuthInfo: authInfo,
-  //   })
-  //   .then((response) => {
-  //     const token = response.data.JWT;
-  //     // console.log("token:", response.data.JWT);
-  //     const config = {
-  //       headers: { Authorization: `Bearer ${token}` },
-  //     };
-  //     const reqbody={
-  //       GroupPath: "",
-  //       Attributes: {
-  //         profile: "standard",
-  //       },
-  //       Login: email,
-  //       Password: pass,
-  //     };
-  //     axios
-  //       .put(
-  //         url,
-  //         reqbody,
-  //         config
-  //       )
-  //       .then((response) => {
-  //         console.log(response.data);
-  //         return res.status(201).send({message:"User Created"});
+  const tokenUrl = "https://storage.dataspace.mobi/a/frontend/session";
+  const url = "https://storage.dataspace.mobi/a/user/upperwal";
+  const login = process.env.login;
+  const password = process.env.password;
+  const type = process.env.type;
+  const authInfo = { login, password, type };
+  axios
+    .post(tokenUrl, {
+      AuthInfo: authInfo,
+    })
+    .then((response) => {
+      const token = response.data.JWT;
+      // console.log("token:", response.data.JWT);
+      const config = {
+        headers: { Authorization: `Bearer ${token}` },
+      };
+      const reqbody={
+        GroupPath: "",
+        Attributes: {
+          profile: "standard",
+        },
+        Login: email,
+        Password: pass,
+      };
+      axios
+        .put(
+          url,
+          reqbody,
+          config
+        )
+        .then((response) => {
+          console.log(response.data);
+          return res.status(201).send({message:"User Created"});
 
-  //       })
-  //       .catch((err) => {
-  //         console.log('user creation error:',err.response.data);
-  //         const error= err.response.datal
-  //         return res.status(500).send({message:error});
-  //       });
-  //   })
-  //   .catch((err) => {
-  //     console.log('token error:',err);
-  //     return res.status(500).send({message:err.response.data});
-  //   });
+        })
+        .catch((err) => {
+          console.log('user creation error:',err.response.data);
+          const error= err.response.datal
+          return res.status(500).send({message:error});
+        });
+    })
+    .catch((err) => {
+      console.log('token error:',err);
+      return res.status(500).send({message:err.response.data});
+    });
 
   // Send email to User
   EmailController.sendVerificationEmail(
