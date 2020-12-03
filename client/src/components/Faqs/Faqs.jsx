4d8b74d8d30826287c7f032693f7cfb8185ef460
Abhishek Upperwal <a.upperwal@gmail.com>
2020-12-03 22:09:52 +0530

    updated deadline

--- client/src/components/Faqs/Faqs.jsx
+++ client/src/components/Faqs/Faqs.jsx
@@ -43,7 +43,7 @@ function Faqs() {
               <div className="col-md-10 align-left top2">
                 <h5>When is the last date of registration?</h5>
                 <p>
-                  The last date of registration 1st January 2021. Though we highly suggest registering in advance so that you have sufficient time to carefully fill the concept note proposal.
+                  The last date of registration 3rd January 2021. Though we highly suggest registering in advance so that you have sufficient time to carefully fill the concept note proposal.
                 </p>
               </div>
             </div>
@@ -53,7 +53,7 @@ function Faqs() {
               <div className="col-md-10 align-left top2">
                 <h5>When is the last date of Concept Note submission?</h5>
                 <p>
-                  The last date of concept note submission is 1st January 2021 
+                  The last date of concept note submission is 3rd January 2021 
                 </p>
               </div>
             </div>
@@ -306,9 +306,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">I am an independent researcher not enrolled in any university or educational institute. Can I take part in the smart move challenge?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>I am an independent researcher not enrolled in any university or educational institute. Can I take part in the smart move challenge?</h5>
                 <p>
                   Yes, independent researchers are eligible to participate. The challenge is divided into the following categories
                 </p>
@@ -321,9 +321,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">I am a professional designer and have founded a design studio which works on projects in improving the mobility experience. How can I participate in the challenge as researchers?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>I am a professional designer and have founded a design studio which works on projects in improving the mobility experience. How can I participate in the challenge as researchers?</h5>
                 <p>
                   Yes, startups and private organizations are eligible to participate.
                 </p>
@@ -331,9 +331,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">What are qualification criteria for members of a team? Educational? Experience?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>What are qualification criteria for members of a team? Educational? Experience?</h5>
                 <p>
                   The participants should register under the two categories defined in the challenge. The team members should demonstrate having appropriate knowledge required to develop their proposed solution.
                 </p>
@@ -341,9 +341,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">What is the last date for NoC submission?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>What is the last date for NoC submission?</h5>
                 <p>
                   Participants required to submit NOC must do it before the final submission of phase II (i.e. 31 March 2021)
                 </p>
@@ -351,9 +351,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">Can a team of students and faculty members participate? </div>
+              <div className="col-md-1"> </div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>Can a team of students and faculty members participate?</h5>
                 <p>
                   Yes, a team consisting of faculty and students can participate provided the team has a maximum of one faculty member. 
                 </p>
@@ -361,9 +361,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">Can students take help from professionals?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>Can students take help from professionals?</h5>
                 <p>
                   The data challenge includes mentorship sessions to shortlisted entries. In case the participants seek guidance from professionals at any stage of challenge they are free to do so. However, such support is purely outside the Challenge on personal capacity cannot be acknowledged in your application. 
                 </p>
@@ -371,9 +371,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">As a smart city, is our role only restricted to sharing data set from different sectors that we may have? </div>
+              <div className="col-md-1"> </div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>As a smart city, is our role only restricted to sharing data set from different sectors that we may have?</h5>
                 <p>
                   Cities are at the center of the challenge. Their association with the challenge is throughout all the stages. Apart from sharing datasets they will be included as a part of mentorship sessions and will assess and help the participants develop their idea to address the specific issues of the city. 
                 </p>
@@ -382,9 +382,9 @@ function Faqs() {
 
 
             <div className="row">
-              <div className="col-md-1">What is the recommended format for the submission of the Concept Note? Is there a limit to word count, file size, etc? </div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>What is the recommended format for the submission of the Concept Note? Is there a limit to word count, file size, etc? </h5>
                 <p>
                   The Concept Note to be submitted in the specified format. Once you create an account on the Smart Move Portal and Login to your workspace you can download the Concept Note Template and other associated guidance documents.
                 </p>
@@ -392,9 +392,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">I have worked on network geography and did some research during my Master's thesis. Does Network theories come under the ambit of research?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>I have worked on network geography and did some research during my Master's thesis. Does Network theories come under the ambit of research?</h5>
                 <p>
                   The proposal is to be submitted in one of the Challenge theme areas as given in the Challenge Guidelines. Please select the closest theme while submitting the Concept Note. 
                 </p>
@@ -402,18 +402,18 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">I have completed my Master's degree in Urban Planning in 2020. My thesis was related to E-mobility. Am I eligible to take part?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>I have completed my Master's degree in Urban Planning in 2020. My thesis was related to E-mobility. Am I eligible to take part?</h5>
                 <p>
                   Yes, you are eligible.
                 </p>
               </div>
             </div>
             <div className="row">
-              <div className="col-md-1">Can you elaborate the qualifications required to participate as a researcher?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>Can you elaborate the qualifications required to participate as a researcher?</h5>
                 <p>
                   You should hold a University Degree and work either as a Researcher with a University or with any Research Institute.
                 </p>
@@ -421,9 +421,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">The proposition is while making CMP we generally tend to ignore how the road network is developing which ultimately leads to haphazard development! So can we think in that direction too? </div>
+              <div className="col-md-1"> </div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>The proposition is while making CMP we generally tend to ignore how the road network is developing which ultimately leads to haphazard development! So can we think in that direction too?</h5>
                 <p>
                   Participants have the liberty to propose and develop implementable and scalable ideas for any issues related to the mobility sector. Though it is mandatory that the participants identify and assign their idea to the theme and sub theme that matches the most with their proposed solution. 
                 </p>
@@ -431,9 +431,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">Do we need to submit our concept note related to the problems from the submitted data or we can use some of our additional data of a city and present a concept note related to its problem?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>Do we need to submit our concept note related to the problems from the submitted data or we can use some of our additional data of a city and present a concept note related to its problem?</h5>
                 <p>
                   Participants are free to address any issue of the mobility sector but it is mandatory that the participants identify and assign their idea to the theme and sub theme that matches the most with their proposed solution. Participants are free to use their own data but they need to inform the organizers about the source of data and its authenticity. Further the data used should either be in the public domain or the participant should have ownership or rights to use that data without any third-party conflict.
                 </p>
@@ -441,9 +441,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">Is the participation and proposal just regarding giving data? Or more into solutions in design and planning level? </div>
+              <div className="col-md-1"> </div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>Is the participation and proposal just regarding giving data? Or more into solutions in design and planning level?</h5>
                 <p>
                   This is not a simple open ideas competition, implementability is key during the whole process. Solutions that address design and planning issues are welcomed. Please refer to the Guidelines and FAQ’s for further clarity.
                 </p>
@@ -451,9 +451,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">Can the idea be about financing of the projects?</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>Can the idea be about financing of the projects?</h5>
                 <p>
                   The proposal is to be submitted in one of the Challenge theme areas as given in the Challenge Guidelines. Please select the closest theme while submitting the Concept Note. 
                 </p>
@@ -461,9 +461,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">Coming to cities of north eastern states we have very limited sources of data. However, it is requested to encourage the participants to take up hilly cities wherein in scenario is different compared to cities from main land.</div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>Coming to cities of north eastern states we have very limited sources of data. However, it is requested to encourage the participants to take up hilly cities wherein in scenario is different compared to cities from main land.</h5>
                 <p>
                   We are determined to provide unique and relevant data sets from different sources within the reach to assist the participants with their requirement. We welcome participants from all the geographical regions of the country and address any issue they find relevant to the mobility sector. You can bring your own data as per Challenge guideline or request what data you would need to support your proposal and we will explore the possibility to provide the data if such data exists. 
                 </p>
@@ -471,9 +471,9 @@ function Faqs() {
             </div>
 
             <div className="row">
-              <div className="col-md-1">In how much detail do we need to present the concept note? </div>
+              <div className="col-md-1"></div>
               <div className="col-md-10 align-left top2">
-                <h5></h5>
+                <h5>In how much detail do we need to present the concept note? </h5>
                 <p>
                   The Concept Note to be submitted in the specified format. Once you create an account on the Smart Move Portal and Login to your workspace you can download the Concept Note Template and other associated guidance documents.
                 </p>
