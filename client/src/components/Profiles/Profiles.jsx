ccb56d8d4b998af666e29d66c2bcc2f2a858e95b
Rishabh <rishabh.sisodiya4@gmail.com>
2021-08-16 20:56:29 +0530

    change timeline

--- client/src/components/Profiles/Profiles.jsx
+++ client/src/components/Profiles/Profiles.jsx
@@ -9,52 +9,31 @@ import profile7 from "./Smart move_Team 7.jpg";
 import profile8 from "./Smart move_Team 8.jpg";
 import profile9 from "./Smart move_Team 9.jpg";
 import profile10 from "./Smart move_Team 10.jpg";
-import { useState } from "react";
-import Carousel from "react-bootstrap/Carousel";
 import "./Profiles.scss";
 
 const Profiles = () => {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
   return (
-    <div className="profile">
-      <Carousel activeIndex={index} onSelect={handleSelect} >
-        <Carousel.Item>
+    <section className="Faqs top">
+      {/* <section className="looking"> */}
+      <div className="container">
+        <div className="heading heading-yellow heading-center top">
+          <h1>Profiles</h1>
+        </div>
+        <div style={{marginTop:"20px",marginBottom:"20px"}}>
           <img className="d-block w-100" src={profile1} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile2} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile3} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile4} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile5} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile6} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile7} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile8} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile9} alt="Team Profile" />
-        </Carousel.Item>
-        <Carousel.Item>
           <img className="d-block w-100" src={profile10} alt="Team Profile" />
-        </Carousel.Item>
-      </Carousel>
-    </div>
+        </div>
+      </div>
+      {/* </section> */}
+    </section>
   );
 };
 
